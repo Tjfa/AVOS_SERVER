ccf678af22383ae4eafd5a2369b1e6761c8f0d4f
QiuFeng <qfviolethill@163.com>
2014-10-24 18:37:59 +0800

    update cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,13 @@ var Match = AV.Object.extend("Match");
 var Player = AV.Object.extend("Player");
 var Team = AV.Object.extend("Team");
 
+AV.Cloud.define("hello", function(request, response) {
+    console.log("hello");
+    response.success("success");
+});
+
 AV.Cloud.define("updateMatchAndPlayerData", function(request, response) {
+    console.log("fucntion : updateMatchAndPlayerData");
     var teamA;
     var teamB;
     var match = request.params.match;
@@ -13,9 +19,9 @@ AV.Cloud.define("updateMatchAndPlayerData", function(request, response) {
     queryA.limit = 1;
     queryA.find({
         success: function(results) {
-
             if (results.length == 0) {
-                response.error(request.params.teamAName + "没有找到");
+                response.error(match.teamAName + "没有找到");
+                return;
             }
             teamA = results[0];
 
@@ -25,9 +31,11 @@ AV.Cloud.define("updateMatchAndPlayerData", function(request, response) {
             queryB.limit = 1;
 
             queryB.find({
+
                 success: function(results) {
                     if (results.length == 0) {
-                        response.error(request.params.teamBName + "没有找到");
+                        response.error(match.teamBName + "没有找到");
+                        return;
                     }
                     teamB = results[0];
 
@@ -37,96 +45,85 @@ AV.Cloud.define("updateMatchAndPlayerData", function(request, response) {
                     var scoreB = parseInt(match.scoreB);
                     var penaltyA = parseInt(match.penaltyA);
                     var penaltyB = parseInt(match.penaltyB);
-                    avMatch.set("scoreA",scoreA);
-                    avMatch.set("scoreB",scoreB);
-                    avMatch.set("penaltyA",penaltyA);
+                    var competitionId = parseInt(match.competitionId);
+
+                    avMatch.set("scoreA", scoreA);
+                    avMatch.set("scoreB", scoreB);
+                    avMatch.set("penaltyA", penaltyA);
                     avMatch.set('penaltyA', penaltyA);
                     avMatch.set('penaltyB', penaltyB);
-                    avMatch.set('teamAId' , teamA.teamId);
-                    avMatch.set('teamBId' , teamB.teamId);
-                    avMatch.isStart = 2;
-                    avMatch.matchProperty = parseInt(match.matchProperty);
-                    avMatch.competitionId = match.competitionId;
-                    avMatch.hint = match.hint;
-                    avMatch.date = string2Date(match.date);
+                    avMatch.set('teamAId', teamA.get('teamId'));
+                    avMatch.set('teamBId', teamB.get('teamId'));
+                    avMatch.set('isStart', 2);
+                    avMatch.set('matchProperty', parseInt(match.matchProperty));
+                    avMatch.set('competitionId', competitionId);
+                    avMatch.set('hint', match.hint);
+                    avMatch.set('date', string2Date(match.date));
                     avMatch.save();
 
                     // 更新team
-                    if (avMatch.matchProperty == 0) {
-                        teamA.groupGoalCount += scoreA;
-                        teamB.groupGoalCount += scoreB;
-                        teamA.groupMissCount += scoreB;
-                        teamB.groupMissCount += scoreA;
+                    if (parseInt(match.matchProperty) == 0) {
+                        teamA.set('groupGoalCount', teamA.get('groupGoalCount') + scoreA);
+                        teamB.set('groupGoalCount', teamB.get('groupGoalCount') + scoreB);
+                        teamA.set('groupMissCount', teamA.get('groupMissCount') + scoreB);
+                        teamB.set('groupMissCount', teamB.get('groupMissCount') + scoreA);
                         if (scoreA > scoreB) {
-                            teamA.groupWinCount += 1;
-                            teamB.groupLostCount += 1;
-                            teamA.winCount += 1;
-                            teamB.lostCount += 1;
-                            teamA.score += 3;
+                            teamA.increment('groupWinCount');
+                            teamB.increment('groupLostCount');
+                            teamA.increment('winCount');
+                            teamB.increment('lostCount');
+                            teamA.set('score', teamA.get('score') + 3);
+
                         } else if (scoreA == scoreB) {
-                            teamA.groupDrawCount += 1;
-                            teamB.groupDrawCount += 1;
-                            teamA.score += 1;
-                            teamB.score += 1;
+                            teamA.increment('groupDrawCount');
+                            teamB.increment('groupDrawCount');
+                            teamA.increment('score');
+                            teamB.increment('score');
+
                         } else {
-                            teamA.groupLostCount += 1;
-                            teamB.groupWinCount += 1;
-                            teamB.winCount += 1;
-                            teamA.lostCount += 1;
-                            teamB.score += 3;
+                            teamA.increment('groupLostCount');
+                            teamA.increment('lostCount');
+                            teamB.increment('groupWinCount');
+                            teamB.increment('winCount');
+                            teamB.set('score', teamB.get('score') + 3);
                         }
-                        teamA.rank = 0;
-                        teamB.rank = 0;
+                        teamA.set('rank', 0);
+                        teamB.set('rank', 0);
+
 
                     } else { //不是小组赛 需要更新rank
                         if (scoreA > scoreB || penaltyA > penaltyB) {
-                            teamA.winCount += 1;
-                            teamB.lostCount += 1;
-                            teamA.rank = getWinRankWithMatchProperty(avMatch.matchProperty);
-                            teamB.rank = getLostRankWithMatchProperty(avMatch.matchProperty);
+                            teamA.increment('winCount');
+                            teamB.increment('lostCount');
+                            teamA.set('rank', getWinRankWithMatchProperty(avMatch.matchProperty));
+                            teamB.set('rank', getLostRankWithMatchProperty(avMatch.matchProperty));
+
                         } else {
-                            teamA.lostCount += 1;
-                            teamB.winCount += 1;
-                            teamA.rank = getLostRankWithMatchProperty(avMatch);
-                            teamB.rank = getWinRankWithMatchProperty(avMatch);
+                            teamA.increment('lostCount');
+                            teamB.increment('winCount');
+                            teamA.set('rank', getLostRankWithMatchProperty(avMatch.matchProperty));
+                            teamB.set('rank', getWinRankWithMatchProperty(avMatch.matchProperty))
                         }
                     }
 
-                    teamA.goalCount += scoreA;
-                    teamB.goalCount += scoreB;
-                    teamA.missCount += scoreB;
-                    teamB.missCount += scoreA;
+                    teamA.set('goalCount', teamA.get('goalCount') + scoreA);
+                    teamB.set('goalCount', teamB.get('goalCount') + scoreB);
+                    teamA.set('missCount', teamA.get('missCount') + scoreB);
+                    teamB.set('missCount', teamB.get('missCount') + scoreA);
                     teamA.save();
                     teamB.save();
 
+                    var players = request.params.players;
 
-                    // for (var player in request.parames.players) {
-                    //     var queryPlayer = new AV.Query("Team");
-                    //     queryPlayer.equalTo("name", player.name);
+                    for (var index in players) {
+                        var player = players[index];
+                        var teamId;
+                        if (player.team == teamA.get('name')) {
+                            teamId = teamA.get('teamId');
+                        } else teamId = teamB.get('teamId');
 
-                    //     var teamId;
-                    //     if (player.team == teamA.name) {
-                    //         teamId = teamA.teamId;
-                    //     } else teamId = teamB.teamId;
-
-                    //     queryPlayer.equalTo("teamId", teamId);
-                    //     queryPlayer.equalTo("competitionId", match.competitionId);
-                    //     queryPlayer.limit = 1;
-                    //     queryPlayer.find({
-                    //         success: function(results) {
-                    //             var avPlayer;
-                    //             if (results.length == 0) {
-                    //                 avPlayer = new Player();
-                    //                 initPlayer(avPlayer);
-                    //             } else avPlayer = results[0];
-
-                    //             avPlayer.goalCount += player.goalCount;
-                    //             avPlayer.yellowCard += player.yellowCard;
-                    //             avPlayer.redCard = player.redCard;
-                    //             avPlayer.save();
-                    //         }
-                    //     });
-                    // }
+                        updatePlayer(player, teamId, competitionId);
+                    }
 
                     var responseObject = {
                         success: "success"
@@ -134,17 +131,54 @@ AV.Cloud.define("updateMatchAndPlayerData", function(request, response) {
                     response.success(responseObject);
                 },
                 error: function() {
-                    response.error(request.parames.teamBName + "没有找到");
+                    response.error(match.teamBName + "没有找到");
                 },
             })
 
         },
         error: function() {
 
-            response.error(request.parames.teamAName + "没有找到");
+            response.error(match.teamAName + "没有找到");
         },
     });
 });
+
+function updatePlayer(player, teamId, competitionId, callback) {
+    var queryPlayer = new AV.Query("Player");
+    queryPlayer.equalTo("name", player.name);
+    queryPlayer.equalTo("teamId", teamId);
+    queryPlayer.equalTo("competitionId", competitionId);
+
+
+    queryPlayer.limit = 1;
+    queryPlayer.find({
+        success: function(results) {
+            var avPlayer;
+            if (results.length == 0) {
+                avPlayer = new Player();
+                initPlayer(avPlayer);
+            } else avPlayer = results[0];
+            avPlayer.set('name', player.name);
+            avPlayer.set('goalCount', avPlayer.get('goalCount') + parseInt(player.goalCount));
+            avPlayer.set('yellowCard', avPlayer.get('yellowCard') + parseInt(player.yellowCard));
+            avPlayer.set('redCard', avPlayer.get('redCard') + parseInt(player.redCard));
+            avPlayer.set('teamId', teamId);
+            avPlayer.set('competitionId', competitionId);
+            avPlayer.save();
+            if (callback) {
+                callback();
+            }
+
+        }
+    });
+
+}
+
+function initPlayer(avPlayer) {
+    avPlayer.set('goalCount', 0);
+    avPlayer.set('yellowCard', 0);
+    avPlayer.set('redCard', 0);
+}
 
 ///    var s = "2005-12-15  09:41:30";
 
@@ -157,7 +191,7 @@ function string2Date(formatString) {
  *  比赛性质 0:小组赛  1:决赛 2:半决赛 4:1/4 8:1/8 16:1/16 32:1/32 100：附加赛
  */
 function getWinRankWithMatchProperty(matchProperty) {
-    if (matchProperty ==100)
+    if (matchProperty == 100)
         return 0;
     return matchProperty;
 }
@@ -183,10 +217,4 @@ function getLostRankWithMatchProperty(matchProperty) {
         default:
             return 0;
     }
-}
-
-function initPlayer(player) {
-    player.goalCount = 0;
-    player.yellowCard = 0;
-    player.redCard = 0;
 }
